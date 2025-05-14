4323b72813a56944fe8cd4b70443be242cf28e04
KennyNeutron <kennyneutron.hex@gmail.com>
2025-05-14 17:22:23 +0800

    added DFRobot MP3 Player function

---
+++ DFRobot_DFPlayer_TestCode/DFRobot_DFPlayer_TestCode.ino
@@ -0,0 +1,48 @@
+/*
+  ----------------------------------------------------------------------
+  Project: Phonics Board Game - DFPlayer Mini Integration
+  Purpose: Play MP3 files using DFPlayer Mini for phonics sounds.
+
+  Board: Arduino Uno (or compatible)
+  Author: KennyNeutron
+  License: GNU Lesser General Public License
+  Date: May 2025
+
+  Description:
+    Plays MP3 files from DFPlayer Mini using SoftwareSerial.
+    Automatically plays next file every 3 seconds.
+
+  ----------------------------------------------------------------------
+*/
+
+#include <SoftwareSerial.h>
+#include "Arduino.h"
+#include "DFRobotDFPlayerMini.h"
+
+// Use SoftwareSerial for DFPlayer communication
+SoftwareSerial FPSerial(10, 11); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
+
+void printDetail(uint8_t type, int value);
+void setup() {
+  FPSerial.begin(9600);
+  Serial.begin(115200);
+
+  Serial.println(F("Initializing DFPlayer..."));
+  if (!myDFPlayer.begin(FPSerial, true, true)) {
+    Serial.println(F("DFPlayer init failed!"));
+    while (true) delay(0);
+  }
+
+  myDFPlayer.volume(25); // Set volume (0–30)
+  Serial.println(F("DFPlayer Mini is ready."));
+  delay(1000);
+  Serial.println("Playing 05.mp3");
+  myDFPlayer.play(5); // Plays 005.mp3
+}
+
+void loop() {
+
+
+
+}
